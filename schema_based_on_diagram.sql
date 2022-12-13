29a833b2e20a1e5ea3a433da2cd115f2ca299b97
Jonathan Kayizzi <jonahkayizzi@gmail.com>
2022-12-13 22:52:18 +0300

    Add patients and medical_histories tables

---
+++ schema_based_on_diagram.sql
@@ -0,0 +1,11 @@
+CREATE TABLE patients (
+    id BIGSERIAL NOT NULL PRIMARY KEY,
+    name VARCHAR(100),
+    date_of_birth DATE
+);
+CREATE TABLE medical_histories(
+    id BIGSERIAL NOT NULL PRIMARY KEY,
+    admitted_at TIMESTAMP,
+    patient_id INT REFERENCES patients(id) ON DELETE CASCADE,
+    status VARCHAR(100)
+);
